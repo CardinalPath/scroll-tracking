0be7fab3e04dc81c69821a463bcbd0d777981020
btripple <btripple@cardinalpath.com>
2022-11-22 16:32:16 -0800

    Update scroll-gtm.js

--- scroll-gtm.js
+++ scroll-gtm.js
@@ -3,7 +3,7 @@
   * Scroll tracking plugin for GTM
   * Description - this code was derived from the Google On Steroid plugin: https://github.com/CardinalPath/gas/blob/master/src/plugins/max_scroll.js
   * Author - Bill Tripple, bill.tripple@cardinalpath.com
-  * Version - 5, last update Nov 21, 2022
+  * Version - 5, last update Nov 22, 2022
 */
 (function(){
 	var CP_ScrollTracking = (function () {
@@ -11,14 +11,16 @@
 		var SETTINGS={
             "event_name":"scroll",
             "percent_scrolled_parameter":"percent_scrolled",
-            "percentage_threshold":100  // this disables scroll tracking when the viewable space is not much greater than the full space, a value of "100" will always track.            
+            "percentage_threshold":100,  // this disables scroll tracking when the viewable space is not much greater than the full space, a value of "100" will always track.   
+            "pageview_delay":100, // delay in milliseconds to wait for content to render on pageload events
+            "spa_pageview_delay":1500, // delay in milliseconds to wait for content to render on historyChange events
 		}     
 		/* configuration ends */
       
        /* advanced settings - change with caution */
-        SETTINGS.debug=true; // set to "false" in production environments.
+        SETTINGS.debug=false; // set to "false" in production environments.
        if(/(\?|\&)debug/.test(document.location.search)){
-          SETTINGS.debug=true; 
+          SETTINGS.debug=false; 
         }
         SETTINGS.scroll_grouping="25";  // changing may lead to errors.
         SETTINGS.trigger_method="event" // setting to "page_unload" will fire event once per pageload.  Useful for reducing event volumes. Not recommended for SPA's.
@@ -73,7 +75,6 @@
            debug_util("checking 75","");
            var _milestones=[25,50];
            for (k = 0; k < _milestones.length; k++) {
-             console.log(_milestones[k])
              if(scroll_history[_milestones[k]]!=true){
               send_backfill(_milestones[k].toString());
               }
@@ -165,26 +166,26 @@
 				init: function(){
                   debug_util("scroll tracking initiated","")
                   var event_name="{{event}}";
-                  var _timeout=1; // trigger immediatly
+                  debug_util("Scroll event","{{event}}")
+                  var _timeout=SETTINGS.pageview_delay; // trigger immediatly
+                  if(event_name=="gtm.historyChange"){
+                      _timeout=SETTINGS.spa_pageview_delay; // add slight delay to wait for it to fully render
+                      debug_util("delay scroll tracking",_timeout);
+                    }
                     setTimeout(function(){   // adding timeout as time required to wait for SPA content to render
                   if(Math.round((window.innerHeight/document.documentElement.scrollHeight)*100)<=SETTINGS.percentage_threshold){
                     debug_util("passed threshold evaluation","");
                     
-                    if(event_name=="gtm.historyChange"){
-                      _timeout=1500; // add slight delay to wait for it to fully render
-                      debug_util("delay scroll tracking",_timeout);
-                    }
-                  
                       addEvent( window, "scroll", _update_scroll_percentage );
                       debug_util("scrollbar event listeners attatched","");
                        if(SETTINGS.trigger_method!="event"){
                             addEvent(window,"unload",_update_scroll_percentage);
                        }
                   }else{
-                    debug_util("failed passed threshold evaluation",SETTINGS.percentage_threshold);
+                    debug_util("failed passed threshold evaluation","threshold:"+SETTINGS.percentage_threshold+"|timeout:"+_timeout);
+                    debug_util("calculated",Math.round((window.innerHeight/document.documentElement.scrollHeight)*100))
                   }
-                    },_timeout); // timeout function ends
-                  
+                    },_timeout); // timeout function ends               
 					return true;
 				},
                getScrollHistory:function(){
